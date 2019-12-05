4cf39899d2b64f684cd3d8eb860a1aeaf32b9de7
Roni Mazouz <rrmazouz@aol.com>
2019-12-05 13:52:55 -0500

    Update scriptwebrtc.js

--- js/scriptwebrtc.js
+++ js/scriptwebrtc.js
@@ -1,12 +1,12 @@
 var connection = new RTCMultiConnection();
 
 // by default, socket.io server is assumed to be deployed on your own URL
-connection.socketURL = 'https://ronimazouz1.github.io:443/';
+connection.socketURL = '/';
 connection.enableFileSharing = true; // by default, it is "false".
 
 
 // comment-out below line if you do not have your own socket.io server
-// connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
+connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
 
 connection.socketMessageEvent = 'Video-Plus-Text-Chat';
 connection.maxParticipantsAllowed=2;
